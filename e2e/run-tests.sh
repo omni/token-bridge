521b8128214cbb7263d05ad0cc0d6a8b0bdf4333
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-06-12 10:39:46 -0300

    Add docker bridges logs

--- e2e/run-tests.sh
+++ e2e/run-tests.sh
@@ -1,10 +1,10 @@
 docker-compose up -d
 docker-compose run e2e npm run deploy
-docker-compose run -d bridge npm run watcher:deposit
-docker-compose run -d bridge npm run watcher:collected-signatures
-docker-compose run -d bridge npm run watcher:withdraw
-docker-compose run -d bridge npm run sender:home
-docker-compose run -d bridge npm run sender:foreign
+docker-compose run --name e2e_bridge_run_1 bridge npm run watcher:deposit &
+docker-compose run --name e2e_bridge_run_2 bridge npm run watcher:collected-signatures &
+docker-compose run --name e2e_bridge_run_3 bridge npm run watcher:withdraw &
+docker-compose run --name e2e_bridge_run_4 bridge npm run sender:home &
+docker-compose run --name e2e_bridge_run_5 bridge npm run sender:foreign &
 docker-compose run e2e npm start
 rc=$?
 docker-compose logs -f -t &
