f977271f3e79d31cc20929dfb58550840c6f4297
Franco Victorio <victorio.franco@gmail.com>
2018-06-18 15:51:18 -0400

    Add file with constants

--- e2e/test/transactions.js
+++ e2e/test/transactions.js
@@ -2,17 +2,16 @@ const path = require('path')
 const Web3 = require('web3')
 const assert = require('assert')
 const promiseRetry = require('promise-retry')
+const { user } = require('../constants.json')
 
 const abisDir = path.join(__dirname, '..', 'submodules/poa-bridge-contracts/build/contracts')
-
-const privateKeyUser = '0x63e48a8ba0b99e0377c6b483af4a072cbca5ffbcfdac77be72e69f4960125800'
 
 const homeWeb3 = new Web3(new Web3.providers.HttpProvider('http://parity1:8545'))
 const foreignWeb3 = new Web3(new Web3.providers.HttpProvider('http://parity2:8545'))
 
 const { toBN } = foreignWeb3.utils
 
-homeWeb3.eth.accounts.wallet.add(privateKeyUser)
+homeWeb3.eth.accounts.wallet.add(user.privateKey)
 
 const tokenAbi = require(path.join(abisDir, 'POA20.json')).abi
 const token = new foreignWeb3.eth.Contract(tokenAbi, '0xdbeE25CbE97e4A5CC6c499875774dc7067E9426B')
@@ -20,14 +19,12 @@ const token = new foreignWeb3.eth.Contract(tokenAbi, '0xdbeE25CbE97e4A5CC6c49987
 describe('transactions', () => {
   it('should convert eth in home to tokens in foreign', async () => {
     // check that account has zero tokens in the foreign chain
-    const balance = await token.methods
-      .balanceOf('0xbb140FbA6242a1c3887A7823F7750a73101383e3')
-      .call()
+    const balance = await token.methods.balanceOf(user.address).call()
     assert(toBN(balance).isZero(), 'Account should not have tokens yet')
 
     // send transaction to home chain
     await homeWeb3.eth.sendTransaction({
-      from: '0xbb140FbA6242a1c3887A7823F7750a73101383e3',
+      from: user.address,
       to: '0x32198D570fffC7033641F8A9094FFDCaAEF42624',
       gasPrice: '1',
       gasLimit: 50000,
@@ -36,9 +33,7 @@ describe('transactions', () => {
 
     // check that account has tokens in the foreign chain
     await promiseRetry(async retry => {
-      const balance = await token.methods
-        .balanceOf('0xbb140FbA6242a1c3887A7823F7750a73101383e3')
-        .call()
+      const balance = await token.methods.balanceOf(user.address).call()
       if (toBN(balance).isZero()) {
         retry()
       }
