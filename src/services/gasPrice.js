0e559f5b852d97eac177737487e0bb5d5069e825
Franco Victorio <victorio.franco@gmail.com>
2018-07-03 17:59:06 -0300

    Use bridge contracts to get gas price if API is not available

--- src/services/gasPrice.js
+++ src/services/gasPrice.js
@@ -1,16 +1,31 @@
 require('dotenv').config()
+const Web3 = require('web3')
 const fetch = require('node-fetch')
+const HomeABI = require('../../abis/HomeBridge.abi')
+const ForeignABI = require('../../abis/ForeignBridge.abi')
 
 const {
-  HOME_GAS_PRICE_FALLBACK,
-  HOME_GAS_PRICE_SPEED_TYPE,
-  HOME_GAS_PRICE_UPDATE_INTERVAL,
-  HOME_GAS_PRICE_ORACLE_URL,
+  FOREIGN_BRIDGE_ADDRESS,
   FOREIGN_GAS_PRICE_FALLBACK,
+  FOREIGN_GAS_PRICE_ORACLE_URL,
   FOREIGN_GAS_PRICE_SPEED_TYPE,
   FOREIGN_GAS_PRICE_UPDATE_INTERVAL,
-  FOREIGN_GAS_PRICE_ORACLE_URL
+  FOREIGN_RPC_URL,
+  HOME_BRIDGE_ADDRESS,
+  HOME_GAS_PRICE_FALLBACK,
+  HOME_GAS_PRICE_ORACLE_URL,
+  HOME_GAS_PRICE_SPEED_TYPE,
+  HOME_GAS_PRICE_UPDATE_INTERVAL,
+  HOME_RPC_URL
 } = process.env
+
+const homeProvider = new Web3.providers.HttpProvider(HOME_RPC_URL)
+const web3Home = new Web3(homeProvider)
+const homeBridge = new web3Home.eth.Contract(HomeABI, HOME_BRIDGE_ADDRESS)
+
+const foreignProvider = new Web3.providers.HttpProvider(FOREIGN_RPC_URL)
+const web3Foreign = new Web3(foreignProvider)
+const foreignBridge = new web3Foreign.eth.Contract(ForeignABI, FOREIGN_BRIDGE_ADDRESS)
 
 const cache = {
   home: HOME_GAS_PRICE_FALLBACK,
@@ -33,6 +48,12 @@ async function start() {
       gasPrice = json[HOME_GAS_PRICE_SPEED_TYPE]
     } catch (e) {
       console.error('Gas Price API is not available', e)
+
+      try {
+        gasPrice = await homeBridge.methods.gasPrice().call()
+      } catch (e) {
+        console.error('There was a problem getting the gas price from the contract', e)
+      }
     }
     cache.home = gasPrice
   }, HOME_GAS_PRICE_UPDATE_INTERVAL)
@@ -46,6 +67,12 @@ async function start() {
       gasPrice = json[FOREIGN_GAS_PRICE_SPEED_TYPE]
     } catch (e) {
       console.error('Gas Price API is not available', e)
+
+      try {
+        gasPrice = await foreignBridge.methods.gasPrice().call()
+      } catch (e) {
+        console.error('There was a problem getting the gas price from the contract', e)
+      }
     }
     cache.foreign = gasPrice
   }, FOREIGN_GAS_PRICE_UPDATE_INTERVAL)
