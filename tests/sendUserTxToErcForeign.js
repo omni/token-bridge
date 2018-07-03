30469402ac10892f5a0ccbb25cb595ad3cbafa71
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-07-03 14:26:17 -0300

    Add scripts to send transactions to bridge

---
+++ tests/sendUserTxToErcForeign.js
@@ -0,0 +1,90 @@
+require('dotenv').config()
+const Web3 = require('web3')
+const Web3Utils = require('web3-utils')
+const { sendTx, sendRawTx } = require('../src/tx/sendTx')
+
+const {
+  USER_ADDRESS,
+  USER_ADDRESS_PRIVATE_KEY,
+  ERC_FOREIGN_BRIDGE_ADDRESS,
+  FOREIGN_RPC_URL,
+  FOREIGN_MIN_AMOUNT_PER_TX,
+  ERC20_TOKEN_ADDRESS,
+  NUMBER_OF_DEPOSITS_TO_SEND
+} = process.env
+
+const ERC20_ABI = [
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_to',
+        type: 'address'
+      },
+      {
+        name: '_value',
+        type: 'uint256'
+      }
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        name: '',
+        type: 'bool'
+      }
+    ],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  }
+]
+
+const foreignProvider = new Web3.providers.HttpProvider(FOREIGN_RPC_URL)
+const web3Foreign = new Web3(foreignProvider)
+
+const poa20 = new web3Foreign.eth.Contract(ERC20_ABI, ERC20_TOKEN_ADDRESS)
+
+async function main() {
+  try {
+    const foreignChaindId = await sendRawTx({
+      url: FOREIGN_RPC_URL,
+      params: [],
+      method: 'net_version'
+    })
+    let nonce = await sendRawTx({
+      url: FOREIGN_RPC_URL,
+      method: 'eth_getTransactionCount',
+      params: [USER_ADDRESS, 'latest']
+    })
+    nonce = Web3Utils.hexToNumber(nonce)
+    let actualSent = 0
+    for (let i = 0; i < Number(NUMBER_OF_DEPOSITS_TO_SEND); i++) {
+      const gasLimit = await poa20.methods
+        .transfer(ERC_FOREIGN_BRIDGE_ADDRESS, Web3Utils.toWei(FOREIGN_MIN_AMOUNT_PER_TX))
+        .estimateGas({ from: USER_ADDRESS })
+      const data = await poa20.methods
+        .transfer(ERC_FOREIGN_BRIDGE_ADDRESS, Web3Utils.toWei(FOREIGN_MIN_AMOUNT_PER_TX))
+        .encodeABI({ from: USER_ADDRESS })
+      const txHash = await sendTx({
+        rpcUrl: FOREIGN_RPC_URL,
+        privateKey: USER_ADDRESS_PRIVATE_KEY,
+        data,
+        nonce,
+        gasPrice: '1',
+        amount: '0',
+        gasLimit,
+        to: ERC20_TOKEN_ADDRESS,
+        web3: web3Foreign,
+        chainId: foreignChaindId
+      })
+      if (txHash !== undefined) {
+        nonce++
+        actualSent++
+        console.log(actualSent, ' # ', txHash)
+      }
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+main()
--- tests/sendUserTxToErcHome.js
+++ tests/sendUserTxToErcHome.js
@@ -6,14 +6,14 @@ const { sendTx, sendRawTx } = require('../src/tx/sendTx')
 const {
   USER_ADDRESS,
   USER_ADDRESS_PRIVATE_KEY,
-  FOREIGN_BRIDGE_ADDRESS,
-  FOREIGN_RPC_URL,
-  FOREIGN_MIN_AMOUNT_PER_TX,
-  POA20_ADDRESS,
+  ERC_HOME_BRIDGE_ADDRESS,
+  HOME_RPC_URL,
+  HOME_MIN_AMOUNT_PER_TX,
+  BRIDGEABLE_TOKEN_ADDRESS,
   NUMBER_OF_WITHDRAWALS_TO_SEND
 } = process.env
 
-const POA20_ABI = [
+const BRIDGEBLE_TOKEN_ABI = [
   {
     constant: false,
     inputs: [
@@ -43,42 +43,42 @@ const POA20_ABI = [
   }
 ]
 
-const foreignProvider = new Web3.providers.HttpProvider(FOREIGN_RPC_URL)
-const web3Foreign = new Web3(foreignProvider)
+const homeProvider = new Web3.providers.HttpProvider(HOME_RPC_URL)
+const web3Home = new Web3(homeProvider)
 
-const poa20 = new web3Foreign.eth.Contract(POA20_ABI, POA20_ADDRESS)
+const erc677 = new web3Home.eth.Contract(BRIDGEBLE_TOKEN_ABI, BRIDGEABLE_TOKEN_ADDRESS)
 
 async function main() {
-  const foreignChaindId = await sendRawTx({
-    url: FOREIGN_RPC_URL,
+  const homeChainId = await sendRawTx({
+    url: HOME_RPC_URL,
     params: [],
     method: 'net_version'
   })
   let nonce = await sendRawTx({
-    url: FOREIGN_RPC_URL,
+    url: HOME_RPC_URL,
     method: 'eth_getTransactionCount',
     params: [USER_ADDRESS, 'latest']
   })
   nonce = Web3Utils.hexToNumber(nonce)
   let actualSent = 0
   for (let i = 0; i < Number(NUMBER_OF_WITHDRAWALS_TO_SEND); i++) {
-    const gasLimit = await poa20.methods
-      .transferAndCall(FOREIGN_BRIDGE_ADDRESS, Web3Utils.toWei(FOREIGN_MIN_AMOUNT_PER_TX), '0x')
+    const gasLimit = await erc677.methods
+      .transferAndCall(ERC_HOME_BRIDGE_ADDRESS, Web3Utils.toWei(HOME_MIN_AMOUNT_PER_TX), '0x')
       .estimateGas({ from: USER_ADDRESS })
-    const data = await poa20.methods
-      .transferAndCall(FOREIGN_BRIDGE_ADDRESS, Web3Utils.toWei(FOREIGN_MIN_AMOUNT_PER_TX), '0x')
+    const data = await erc677.methods
+      .transferAndCall(ERC_HOME_BRIDGE_ADDRESS, Web3Utils.toWei(HOME_MIN_AMOUNT_PER_TX), '0x')
       .encodeABI({ from: USER_ADDRESS })
     const txHash = await sendTx({
-      rpcUrl: FOREIGN_RPC_URL,
+      rpcUrl: HOME_RPC_URL,
       privateKey: USER_ADDRESS_PRIVATE_KEY,
       data,
       nonce,
       gasPrice: '1',
       amount: '0',
       gasLimit,
-      to: POA20_ADDRESS,
-      web3: web3Foreign,
-      chainId: foreignChaindId
+      to: BRIDGEABLE_TOKEN_ADDRESS,
+      web3: web3Home,
+      chainId: homeChainId
     })
     if (txHash !== undefined) {
       nonce++
