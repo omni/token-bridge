cc051c211f43de46f000e239470bac6d83258636
Franco Victorio <victorio.franco@gmail.com>
2018-07-23 15:27:08 -0300

    Add tests for syncForEach

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,12 +1,17 @@
 const sinon = require('sinon')
-const { expect } = require('chai')
-const { checkHTTPS } = require('../src/utils/utils')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const { checkHTTPS, syncForEach } = require('../src/utils/utils')
+
+chai.use(chaiAsPromised)
+const { expect } = chai
 
 describe('utils', () => {
   describe('checkHTTPS', () => {
     beforeEach(() => {
       sinon.stub(console, 'warn')
     })
+
     afterEach(() => {
       console.warn.restore()
     })
@@ -28,6 +33,78 @@ describe('utils', () => {
 
     it('should throw an error if HTTP is not allowed and the URL is http', () => {
       expect(() => checkHTTPS('no')('http://www.google.com')).to.throw()
+    })
+  })
+
+  describe('syncForEach', () => {
+    it('should execute callback sequentially', async () => {
+      const xs = []
+      await syncForEach(
+        [1, 2, 3],
+        x =>
+          new Promise(resolve => {
+            xs.push(x)
+            resolve()
+          })
+      )
+
+      expect(xs).to.deep.equal([1, 2, 3])
+    })
+
+    it('should receive the index and the full array', async () => {
+      const xs = []
+      const is = []
+      const arrays = []
+      await syncForEach(
+        [1, 2, 3],
+        (x, i, array) =>
+          new Promise(resolve => {
+            xs.push(x)
+            is.push(i)
+            arrays.push(array)
+            resolve()
+          })
+      )
+
+      expect(xs).to.deep.equal([1, 2, 3])
+      expect(is).to.deep.equal([0, 1, 2])
+      expect(arrays).to.deep.equal([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+    })
+
+    it('should fail and stop when the callback fails', () => {
+      const xs = []
+      const promise = syncForEach(
+        [1, 2, 3],
+        x =>
+          new Promise((resolve, reject) => {
+            if (x === 2) {
+              return reject()
+            }
+
+            xs.push(x)
+            return resolve()
+          })
+      )
+
+      return expect(promise).to.be.rejected.then(() => {
+        expect(xs).to.deep.equal([1])
+      })
+    })
+
+    it('should work with an empty array', () => {
+      const xs = []
+      const promise = syncForEach(
+        [],
+        x =>
+          new Promise(resolve => {
+            xs.push(x)
+            return resolve()
+          })
+      )
+
+      return expect(promise).to.be.fulfilled.then(() => {
+        expect(xs).to.deep.equal([])
+      })
     })
   })
 })
