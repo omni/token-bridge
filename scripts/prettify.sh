b5683a4c1a6b06380128294690499f7ffc78939d
Franco Victorio <victorio.franco@gmail.com>
2018-08-15 11:08:15 -0300

    Add script to start workers

--- scripts/prettify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Prettify the input only if we are not in production
-if [ "$NODE_ENV" = "production" ]
-then
-  cat
-else
-  pino-pretty
-fi
---
+++ scripts/start-worker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+WORKERS_DIR="src/"
+LOGS_DIR="logs/"
+
+WORKER="${WORKERS_DIR}${1}.js"
+CONFIG="${2}.config.js"
+LOG="${LOGS_DIR}${2}.txt"
+
+if [ "${NODE_ENV}" = "production" ]; then
+  exec node "${WORKER}" "${CONFIG}"
+else
+  node "${WORKER}" "${CONFIG}" | tee -a "${LOG}" | pino-pretty
+fi
