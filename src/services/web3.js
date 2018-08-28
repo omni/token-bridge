d0b9a24685ce4a40377d5d1977df76323badd95f
Franco Victorio <victorio.franco@gmail.com>
2018-08-28 15:19:35 -0300

    Add retrying to web3

--- src/services/web3.js
+++ src/services/web3.js
@@ -2,10 +2,18 @@ const HttpListProvider = require('http-list-provider')
 const Web3 = require('web3')
 const rpcUrlsManager = require('./getRpcUrlsManager')
 
-const homeProvider = new HttpListProvider(rpcUrlsManager.homeUrls)
+const homeProvider = new HttpListProvider(rpcUrlsManager.homeUrls, {
+  retry: {
+    retries: 10
+  }
+})
 const web3Home = new Web3(homeProvider)
 
-const foreignProvider = new HttpListProvider(rpcUrlsManager.foreignUrls)
+const foreignProvider = new HttpListProvider(rpcUrlsManager.foreignUrls, {
+  retry: {
+    retries: 10
+  }
+})
 const web3Foreign = new Web3(foreignProvider)
 
 module.exports = {
